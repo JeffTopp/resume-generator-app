83db430bc63ed429846a8e5bbd65d86585f0daa5
JeffTopp <jeff@toppcreeksales.com>
2025-02-03 08:55:27 -0800

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResumeGenerator from './components/ResumeGenerator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>AI Resume Generator</h1>
+      <ResumeGenerator />
+    </div>
+  );
+};
+
+export default App;
